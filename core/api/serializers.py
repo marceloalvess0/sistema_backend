4d1d0a2bc1a59e88aae15be2740b133c20b5ec46
PV <victor1324pv@gmail.com>
2023-05-17 14:11:38 -0300

    atualizando requirements e settings

---
+++ core/api/serializers.py
@@ -0,0 +1,7 @@
+# from rest_framework import serializers
+
+# Exemplo do paulo
+# class UserSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['url','username','groups','email']
---
+++ core/api/viewsets.py
@@ -0,0 +1,5 @@
+# from django.contrib.auth.models import User,Group
+# from rest_framework import viewsets
+# class GroupviewSet(viewsets.ModelViewSet):
+#     serializer_class = UserSerializer
+#     permission_classes = [permissions.IsAuthenticated]
