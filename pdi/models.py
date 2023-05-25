cc6850c429a67b5e51fc92729ce83d2101686069
Marcelo Alves <marcelodossantoalves08@gmail.com>
2023-05-25 10:47:55 -0300

    criação de pdf e função para data

--- pdi/models.py
+++ pdi/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 import os
 
 # Define a pasta para armazenar o arquivo de upload
@@ -12,6 +13,7 @@ def path_upload(instance,filename):
     # Concatena o caminho completo do arquivo
     return os.path.join('uploads', subfolder, filename)
 
+
 class Pdi(models.Model):
     objective_title = models.CharField('Titulo do Objetivo',max_length = 25, null = False, blank = False)
     developed_areas = models.CharField('Áreas ',max_length = 100, null = False, blank = False)
@@ -21,3 +23,9 @@ class Pdi(models.Model):
     file = models.FileField('Arquivo',upload_to=path_upload, blank=True, null=True)
     description = models.TextField('Descrição')
     
+    
+    def clean(self):
+        # Verifica se a data final é anterior à data inicial
+        if self.final_datetime and self.initial_datetime:
+            if self.final_datetime < self.initial_datetime:
+                raise ValidationError('A data final não pode ser anterior à data inicial.')
